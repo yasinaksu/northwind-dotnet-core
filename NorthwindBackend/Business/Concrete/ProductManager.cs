5bee635b2759bd4c9583c47caf8d62e2e2000e34
Yasin Aksu <yasinaksu88@gmail.com>
2020-11-24 11:20:00 +0300

    business layer service and manager for product entity

---
+++ NorthwindBackend/Business/Concrete/ProductManager.cs
@@ -0,0 +1,49 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private readonly IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetById(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Product> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Product> GetListByCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
