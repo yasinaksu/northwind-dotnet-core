c380474e825bfefe00ff2c7197702c1af424c63b
Yasin Aksu <yasinaksu88@gmail.com>
2020-12-02 11:05:15 +0300

    Message management and productscontroller crud

--- NorthwindBackend/Business/Concrete/ProductManager.cs
+++ NorthwindBackend/Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.Constants;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -21,13 +22,13 @@ namespace Business.Concrete
         public IResult Add(Product product)
         {
             _productDal.Add(product);
-            return new SuccessResult("Ürün başarıyla eklendi");
+            return new SuccessResult(Messages.ProductAdded);
         }
 
         public IResult Delete(Product product)
         {
             _productDal.Delete(product);
-            return new SuccessResult("Ürün başarıyla silindi");
+            return new SuccessResult(Messages.ProductDeleted);
         }
 
         public IDataResult<Product> GetById(int productId)
@@ -48,7 +49,7 @@ namespace Business.Concrete
         public IResult Update(Product product)
         {
             _productDal.Update(product);
-            return new SuccessResult("Ürün başarıyla güncellendi");
+            return new SuccessResult(Messages.ProductUpdated);
         }
     }
 }
